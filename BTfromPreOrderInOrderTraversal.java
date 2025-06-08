f2dcd0f345f8921fa9166cfeabdcfbab82f84487
chanukya246 <33529876+chanukya246@users.noreply.github.com>
2025-06-08 11:56:58 -0500

    Completed Trees-1

---
+++ BTfromPreOrderInOrderTraversal.java
@@ -0,0 +1,42 @@
+TC: O(N)
+SC: O(N)
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class BTfromPreOrderInOrderTraversal {
+    int i = 0, p = 0;
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        return building(preorder, inorder, Integer.MIN_VALUE);
+    }
+
+    private TreeNode building(int[] preorder, int[] inorder, int stopValue) {
+
+        if (p >= preorder.length) return null;
+
+        if (inorder[i] == stopValue) {
+            i++;
+            return null;
+        }
+
+        TreeNode node = new TreeNode(preorder[p++]);
+        node.left = building(preorder, inorder, node.val);
+        node.right = building(preorder, inorder, stopValue);
+
+        return node;
+    }
+}
---
+++ ValidateBST_LC98.java
@@ -0,0 +1,38 @@
+TC: O(N)
+SC: O(N)
+
+// each node is bounded by 2 values low & high for ex: -infinity & +infinity
+// for left side traversal to the root , low is always -infinity & high is always root.val
+// for right side traversal to the root , low is always root.val & high is always +-infinity
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class ValidateBST_LC98 {
+    public boolean isValidBST(TreeNode root) {
+
+        return validate(root, null, null);
+    }
+
+    private boolean validate(TreeNode root, Integer low, Integer high) {
+
+        if (root == null) return true;
+        if ((low != null && root.val <= low) || (high != null && root.val >= high)) {
+                return false;
+        }
+
+        return validate(root.left, low, root.val) && validate(root.right, root.val, high);
+    }
+}
